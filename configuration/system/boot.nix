650ec95a484bc77084849a33aef1841271a60816
aleph-complex <89320518+aleph-complex@users.noreply.github.com>
2024-07-12 23:38:42 -0600

    Added secure boot support via Lanzaboote and disabled troublesome network config in security.nix

--- configuration/system/boot.nix
+++ configuration/system/boot.nix
@@ -3,14 +3,14 @@
 {
   # Disable the systemd-boot EFI boot loader
   # It is now managed by Lanzaboote
-  boot.loader.systemd-boot.enable = true; # Temporarily reenabled until I setup secureboot again
+  boot.loader.systemd-boot.enable = false;
   boot.loader.efi.canTouchEfiVariables = true;
 
   # Enable Lanzaboote
-#  boot.lanzaboote = {
-#    enable = true;
-#    pkiBundle = "/etc/secureboot";
-#  };
+  boot.lanzaboote = {
+    enable = true;
+    pkiBundle = "/etc/secureboot";
+  };
 
   # Tooling for secureboot
   environment.systemPackages = [ pkgs.sbctl ];
--- configuration/system/security.nix
+++ configuration/system/security.nix
@@ -3,6 +3,7 @@
 {
   # Enable firewall and block all ports
   networking.firewall.enable = true;
+  networking.firewall.allowPing = false;
   networking.firewall.allowedTCPPorts = [];
   networking.firewall.allowedUDPPorts = [];
 
@@ -20,28 +21,6 @@
   # Enable AppArmor and allow it to kill new programs
   security.apparmor.enable = true;
   security.apparmor.killUnconfinedConfinables = true;
-
-  # Enable strict reverse path filtering
-  boot.kernel.sysctl."net.ipv4.conf.all.log_martians" = true;
-  boot.kernel.sysctl."net.ipv4.conf.all.rp_filter" = "1";
-  boot.kernel.sysctl."net.ipv4.conf.default.log_martians" = true;
-  boot.kernel.sysctl."net.ipv4.conf.default.rp_filter" = "1";
-
-  # Ignore broadcast ICMP
-  boot.kernel.sysctl."net.ipv4.icmp_echo_ignore_broadcasts" = true;
-
-  # Ignore incoming ICMP redirects (note: default is needed to ensure that the
-  # setting is applied to interfaces added after the sysctls are set)
-  boot.kernel.sysctl."net.ipv4.conf.all.accept_redirects" = lib.mkDefault false;
-  boot.kernel.sysctl."net.ipv4.conf.all.secure_redirects" = lib.mkDefault false;
-  boot.kernel.sysctl."net.ipv4.conf.default.accept_redirects" = lib.mkDefault false;
-  boot.kernel.sysctl."net.ipv4.conf.default.secure_redirects" = lib.mkDefault false;
-  boot.kernel.sysctl."net.ipv6.conf.all.accept_redirects" = lib.mkDefault false;
-  boot.kernel.sysctl."net.ipv6.conf.default.accept_redirects" = lib.mkDefault false;
-
-  # Ignore outgoing ICMP redirects
-  boot.kernel.sysctl."net.ipv4.conf.all.send_redirects" = lib.mkDefault false;
-  boot.kernel.sysctl."net.ipv4.conf.default.send_redirects" = lib.mkDefault false;
 
   # Allows full control over all network connections
   # Still deciding if it's a nuisance or worth it
