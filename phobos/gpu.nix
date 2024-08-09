04d1f2e5c7b0917e85d05c1f0aa6ddc02d44a8c3
aleph-complex <89320518+aleph-complex@users.noreply.github.com>
2024-08-08 18:36:29 -0600

    Update flake.lock and disable zluda due to a build issue

--- phobos/gpu.nix
+++ phobos/gpu.nix
@@ -2,6 +2,5 @@
 
 {
   hardware.amdgpu.opencl.enable = true; # Enable OpenGL support via ROCm
-  programs.tuxclocker.enable = true; # Tuning and monitoring
-  environment.systemPackages = [ pkgs.zluda ]; # Enable CUDA support via ZLUDA
+  # environment.systemPackages = [ pkgs.zluda ]; # Enable CUDA support via ZLUDA
 }
